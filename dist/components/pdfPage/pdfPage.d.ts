bd1f794e24bfbbc9e8b16e13d6f824e60ce24e88
Pavel <75898907+GribovskyPavel@users.noreply.github.com>
2024-02-09 16:28:53 +0300

    Implementing tree bbox selection mode (#63)

--- dist/components/pdfPage/pdfPage.d.ts
+++ dist/components/pdfPage/pdfPage.d.ts
@@ -1,11 +1,13 @@
 import React from 'react';
 import { IBbox, IColorScheme, TreeElementBbox } from '../bbox/Bbox';
 import { IPageProps } from './IPageProps';
+import { TreeBboxSelectionMode } from '../../enums/treeBboxSelectionMode';
 import { AnyObject } from '../../types/generics';
 import './pdfPage.scss';
 interface IPdfPageProps extends IPageProps {
     bboxList?: IBbox[];
     treeElementsBboxes?: TreeElementBbox[];
+    treeBboxSelectionMode?: TreeBboxSelectionMode;
     isTreeBboxesVisible: boolean;
     defaultHeight?: number;
     defaultWidth?: number;
