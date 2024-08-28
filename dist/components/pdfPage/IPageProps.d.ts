2ebf2941a480e912b27a325e2ff332921a3f78b6
Ekaterina Chubrick <orient.ks@gmail.com>
2024-08-28 10:46:29 +0300

    Update build

--- dist/components/pdfPage/IPageProps.d.ts
+++ dist/components/pdfPage/IPageProps.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PDFPageProxy, RenderFunction, TextItem } from 'react-pdf/dist/Page';
+import { PDFPageProxy, RenderFunction, TextItem, TextLayerItemInternal } from 'react-pdf/dist/Page';
 import { TSelectedBboxData } from '../../types/selectedBboxData';
 import { OrNull } from '../../types/generics';
 export interface IPageProps {
@@ -26,4 +26,5 @@ export interface IPageProps {
     }): void;
     onGetTextError?(error: Error): void;
     onBboxClick?(data: OrNull<TSelectedBboxData>): void;
+    customTextRenderer?(layer: TextLayerItemInternal): string;
 }
