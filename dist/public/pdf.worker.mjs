5ca1d5f62e67b4013f7e5ac9ca1b584246063e3c
Elizaveta Selyukovich <elizaveta.selyukovich@duallab.com>
2026-06-26 16:50:16 +0300

    Skip hidden bboxed on keydown navigation

--- dist/public/pdf.worker.mjs
+++ dist/public/pdf.worker.mjs
@@ -39705,13 +39705,6 @@ class ExtendedCatalog extends Catalog {
     }
     return shadow(this, "structTreeRootObject", structTreeRoot);
   }
-  convertString(str, type) {
-    try {
-      return stringToUTF8String(str);
-    } catch (e) {
-      return stringToPDFString(str);
-    }
-  }
   getTreeElement(el, page, ref) {
     try {
       if (el instanceof Dict && el.has("Pg")) {
@@ -39726,8 +39719,8 @@ class ExtendedCatalog extends Catalog {
         const name = el.has("S") ? el.get("S").name : null;
         const roleName = this.getRoleName(el, name);
         const treeElement = {
-          name: name ? this.convertString(name, "Name") : null,
-          roleName: roleName ? this.convertString(roleName, "Role name") : null,
+          name: name ? stringToPDFString(name) : null,
+          roleName: roleName ? stringToPDFString(roleName) : null,
           children: this.getTreeElement(el.get("K"), page, el.getRaw("K")),
           pageIndex: page,
           ref: ref instanceof Ref ? ref : null
@@ -39737,7 +39730,7 @@ class ExtendedCatalog extends Catalog {
           alt = el.has("ActualText") ? el.get("ActualText") : null;
         }
         if (typeof alt === "string") {
-          treeElement.alt = this.convertString(alt, "Alt");
+          treeElement.alt = stringToPDFString(alt);
         }
         return treeElement;
       }
@@ -39800,8 +39793,8 @@ class ExtendedCatalog extends Catalog {
         const name = el.get("S").name;
         const roleName = this.getRoleName(el, name);
         const treeElement = {
-          name: name ? this.convertString(name, "Name") : null,
-          roleName: roleName ? this.convertString(roleName, "Role name") : null,
+          name: name ? stringToPDFString(name) : null,
+          roleName: roleName ? stringToPDFString(roleName) : null,
           children: [],
           pageIndex: page,
           ref: ref instanceof Ref ? ref : null
@@ -39811,7 +39804,7 @@ class ExtendedCatalog extends Catalog {
           alt = el.has("ActualText") ? el.get("ActualText") : null;
         }
         if (typeof alt === "string") {
-          treeElement.alt = this.convertString(alt, "Alt");
+          treeElement.alt = stringToPDFString(alt);
         }
         return treeElement;
       }
