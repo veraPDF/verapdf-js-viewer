87f1a21231cfae1945e06040aff47bff648fb5f7
Ekaterina Chubrick <ekaterina.chubrick@duallab.com>
2026-07-13 12:10:23 +0300

    Requested changes

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css';
-
 import PdfViewer from '@duallab/verapdf-js-viewer';
 import {useState, useEffect, useCallback, useMemo} from "react";
 
 import InfoPane from "./InfoPane/InfoPane.jsx";
+
+import './App.css';
 
 import defaultSample from "./assets/PDFUA-in-a-Nutshell-PDFUA.pdf";
 
@@ -68,26 +68,28 @@ function App() {
                     @duallab/verapdf-js-viewer
                 </p>
             </header>
-            <InfoPane documentInfo={documentInfo} numPages={numPages} nodePath={nodePath} />
-            <div className="pdf-container-wrapper">
-                <PdfViewer
-                    className="pdf-container"
-                    scale={1}
-                    file={file}
-                    showAllPages
-                    externalLinkTarget="_blank"
-                    onLoadSuccess={onDocumentLoadSuccess}
-                    treeBboxSelectionMode="SELECTED_WITH_KIDS"
-                    isTreeBboxesVisible
-                    onPageChange={onPageChange}
-                    renderTextLayer={false}
-                    activeBboxId={activeBboxIdData}
-                    activeBboxIndex={activeBboxIndexData}
-                    onBboxClick={onBboxSelect}
-                    onSelectBbox={onBboxSelect}
-                    page={pageNumber}
-                    bboxes={bboxes}
-                />
+            <div className="content-wrapper">
+                <InfoPane documentInfo={documentInfo} numPages={numPages} nodePath={nodePath}/>
+                <div className="pdf-container-wrapper">
+                    <PdfViewer
+                        className="pdf-container"
+                        scale={1}
+                        file={file}
+                        showAllPages
+                        externalLinkTarget="_blank"
+                        onLoadSuccess={onDocumentLoadSuccess}
+                        treeBboxSelectionMode="SELECTED_WITH_KIDS"
+                        isTreeBboxesVisible
+                        onPageChange={onPageChange}
+                        renderTextLayer={false}
+                        activeBboxId={activeBboxIdData}
+                        activeBboxIndex={activeBboxIndexData}
+                        onBboxClick={onBboxSelect}
+                        onSelectBbox={onBboxSelect}
+                        page={pageNumber}
+                        bboxes={bboxes}
+                    />
+                </div>
             </div>
             <footer>
                 <p>Page {pageNumber} of {numPages}</p>
